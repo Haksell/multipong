52e775dd5831a782cc0e4348d17cfbef6d925fe1
Axel Brisse <axel.brisse@gmail.com>
2024-11-19 12:21:48 +0100

    actually kinda works

---
+++ src/client.rs
@@ -0,0 +1,149 @@
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+use futures::StreamExt;
+use game::game_message::Message;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+pub mod game {
+    tonic::include_proto!("game");
+}
+
+use game::game_service_client::GameServiceClient;
+use game::{ControlInput, Direction, GameMessage, GameState};
+
+#[derive(Resource)]
+struct NetworkResource {
+    sender: mpsc::UnboundedSender<GameMessage>,
+    game_state: Arc<Mutex<Option<GameState>>>,
+}
+
+#[derive(Component)]
+struct Paddle {
+    is_left: bool,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Pong".into(),
+                resolution: WindowResolution::new(800., 800.),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        // .init_resource(PlayerId(player_id))
+        .add_systems(Startup, (setup, network_setup))
+        .add_systems(Update, (paddle_input_system, game_state_system))
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2dBundle::default());
+
+    // Left paddle
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(10., 100.)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(-380., 0., 0.),
+            ..Default::default()
+        })
+        .insert(Paddle { is_left: true });
+
+    // Right paddle
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(10., 100.)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(380., 0., 0.),
+            ..Default::default()
+        })
+        .insert(Paddle { is_left: false });
+}
+
+fn network_setup(mut commands: Commands) {
+    let (tx, rx) = mpsc::unbounded_channel::<GameMessage>();
+    let game_state = Arc::new(Mutex::new(None));
+
+    let network_resource = NetworkResource {
+        sender: tx.clone(),
+        game_state: game_state.clone(),
+    };
+
+    commands.insert_resource(network_resource);
+
+    thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async move {
+            let mut client = GameServiceClient::connect("http://[::1]:50051")
+                .await
+                .expect("Failed to connect to server");
+
+            let outbound = UnboundedReceiverStream::new(rx);
+
+            let response = client
+                .play(outbound)
+                .await
+                .expect("Failed to start Play stream");
+
+            let mut inbound = response.into_inner();
+
+            while let Some(Ok(game_msg)) = inbound.next().await {
+                if let Some(Message::GameState(game_state_msg)) = game_msg.message {
+                    let mut state = game_state.lock().unwrap();
+                    *state = Some(game_state_msg);
+                }
+            }
+        });
+    });
+}
+
+fn paddle_input_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    network: ResMut<NetworkResource>,
+) {
+    let direction = if keyboard_input.pressed(KeyCode::KeyW)
+        || keyboard_input.pressed(KeyCode::ArrowUp)
+    {
+        Direction::Up
+    } else if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
+        Direction::Down
+    } else {
+        Direction::None
+    };
+
+    let control_input = ControlInput {
+        player_id: 1,
+        direction: direction as i32,
+    };
+
+    let game_message = GameMessage {
+        message: Some(Message::ControlInput(control_input)),
+    };
+
+    let _ = network.sender.send(game_message);
+}
+
+fn game_state_system(network: Res<NetworkResource>, mut query: Query<(&Paddle, &mut Transform)>) {
+    let state_option = { network.game_state.lock().unwrap().clone() };
+
+    if let Some(game_state) = state_option {
+        for (paddle, mut transform) in query.iter_mut() {
+            if paddle.is_left {
+                transform.translation.y = game_state.left_paddle_y;
+            } else {
+                transform.translation.y = game_state.right_paddle_y;
+            }
+        }
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,99 @@
+use futures::Stream;
+use game::game_message::Message;
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod game {
+    tonic::include_proto!("game");
+}
+
+use game::game_service_server::{GameService, GameServiceServer};
+use game::{Direction, GameMessage, GameState};
+
+type GameMessageStream = Pin<Box<dyn Stream<Item = Result<GameMessage, Status>> + Send>>;
+
+#[derive(Debug, Default)]
+struct SharedGameState {
+    left_paddle_y: f32,
+    right_paddle_y: f32,
+    clients: Vec<mpsc::Sender<Result<GameMessage, Status>>>,
+}
+
+#[derive(Debug, Default)]
+pub struct GameServer {
+    state: Arc<Mutex<SharedGameState>>,
+}
+
+#[tonic::async_trait]
+impl GameService for GameServer {
+    type PlayStream = GameMessageStream;
+
+    async fn play(
+        &self,
+        request: Request<tonic::Streaming<GameMessage>>,
+    ) -> Result<Response<Self::PlayStream>, Status> {
+        let mut stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(32);
+
+        {
+            let mut state = self.state.lock().await;
+            state.clients.push(tx.clone());
+        }
+
+        let state = self.state.clone();
+
+        tokio::spawn(async move {
+            while let Ok(Some(game_msg)) = stream.message().await {
+                if let Some(Message::ControlInput(control_input)) = game_msg.message {
+                    let mut state = state.lock().await;
+                    match control_input.player_id {
+                        1 => match Direction::try_from(control_input.direction) {
+                            Ok(Direction::Up) => state.left_paddle_y += 5.0,
+                            Ok(Direction::Down) => state.left_paddle_y -= 5.0,
+                            _ => (),
+                        },
+                        2 => match Direction::try_from(control_input.direction) {
+                            Ok(Direction::Up) => state.right_paddle_y += 5.0,
+                            Ok(Direction::Down) => state.right_paddle_y -= 5.0,
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+
+                    let game_state = GameMessage {
+                        message: Some(game::game_message::Message::GameState(GameState {
+                            left_paddle_y: state.left_paddle_y,
+                            right_paddle_y: state.right_paddle_y,
+                        })),
+                    };
+
+                    for client in &state.clients {
+                        let _ = client.send(Ok(game_state.clone())).await;
+                    }
+                }
+            }
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(Box::pin(output_stream) as Self::PlayStream))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let game_server = GameServer::default();
+
+    println!("Server listening on {}", addr);
+
+    Server::builder()
+        .add_service(GameServiceServer::new(game_server))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
